e2b3b898ded335c9404b2ecb655cbec81d57d83c
nando <muhammad.rezki.ananda@gmail.com>
2021-12-20 21:23:53 +0700

    SampleEncapsulation

---
+++ src/main/kotlin/SampleEncapsulation.kt
@@ -0,0 +1,38 @@
+class Student{
+    private var nim: String? = null
+    private var name: String? = null
+    private var age: Int? = null
+
+
+    fun getNim(): String? {
+        return nim
+    }
+    fun setNim(value: String){
+        nim = value
+    }
+
+    fun getName(): String? {
+        return name
+    }
+    fun setName(value: String){
+        name = value
+    }
+
+    fun getAge(): Int? {
+        return age
+    }
+    fun setAge(value: Int?){
+        age = value
+    }
+
+
+}
+fun main(){
+    var student = Student()
+    student.setNim("1202190044")
+    student.setName("Rezki")
+    student.setAge(21)
+    println(student.getNim())
+    println(student.getName())
+    println(student.getAge())
+}
